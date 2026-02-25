b46862aee1b7552beaa2a0d8dc878ccd7049bda3
slayernominee <slayernominee_git@proton.me>
2026-02-25 08:22:02 +0100

    v

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -341,7 +341,7 @@ class SettingsScreen extends StatelessWidget {
             ),
             const Center(
               child: Text(
-                'Version 1.8.0',
+                'Version 1.8.1',
                 style: TextStyle(color: Colors.grey),
               ),
             ),
@@ -442,7 +442,7 @@ class SettingsScreen extends StatelessWidget {
                 onTap: () => showLicensePage(
                   context: context,
                   applicationName: 'Lift',
-                  applicationVersion: '1.8.0',
+                  applicationVersion: '1.8.1',
                 ),
               ),
             ),
